46ed38262e8ecced2dd20fa70bcbc1cf0774e7e3
Akn <sibata486@gmail.com>
2024-10-05 23:00:53 +0900

    init

---
+++ MvImage/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvImage.Models;
+using Prism.Mvvm;
+
+namespace MvImage.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private TextWrapper title;
+
+        public TextWrapper Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
