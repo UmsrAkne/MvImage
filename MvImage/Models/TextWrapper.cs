0d981a04a6453a6f8e44d9f9d8901d3d70363317
Akn <sibata486@gmail.com>
2024-10-07 23:21:21 +0900

    Update version number.

--- MvImage/Models/TextWrapper.cs
+++ MvImage/Models/TextWrapper.cs
@@ -34,7 +34,7 @@ namespace MvImage.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20241005" + "a";
+            Version = "20241007" + "a";
         }
 
         [Conditional("DEBUG")]
